fa6d3b58c1f5f7a994dfeb0158346f3d88e1a29b
mdavidrodriguez <mdavidrodriguez@unicesar.edu.co>
2024-01-29 16:06:07 -0500

    Creando filtro propiedades con alberca --en memoria

--- src/composables/usePropiedades.js
+++ src/composables/usePropiedades.js
@@ -1,12 +1,21 @@
+import { computed, ref } from "vue";
 import { collection } from "firebase/firestore";
 import { useFirestore, useCollection } from "vuefire";
 
 export default function usePropiedades() {
-  const db = useFirestore();
+  const alberca = ref(false);
 
+  const db = useFirestore();
   const propiedadesCollection = useCollection(collection(db, "propiedades"));
 
+  const propiedadesFiltradas = computed(() => {
+    return alberca.value
+      ? propiedadesCollection.value.filter((propiedad) => propiedad.alberca)
+      : propiedadesCollection.value;
+  });
   return {
+    alberca,
+    propiedadesFiltradas,
     propiedadesCollection,
   };
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,15 +2,21 @@
 import usePropiedades from '@/composables/usePropiedades'
 import Propiedad from '@/components/Propiedad.vue'
 import { propertyPrice } from '@/helpers'
-const { propiedadesCollection } = usePropiedades()
+const { alberca, propiedadesCollection, propiedadesFiltradas } = usePropiedades()
 
 </script>
 
 <template>
   <h1 class="text-center text-h3 font-weight-bold my-5">Nuestrar Propiedades</h1>
   <v-card flat class="py-10">
+    <v-card-title class="text-h5">
+      Busqueda
+      <v-checkbox label="Alberca" v-model="alberca">
+
+      </v-checkbox>
+    </v-card-title>
     <v-row>
-      <Propiedad v-for="propiedad in propiedadesCollection" :key="propiedad.id" :propiedad="propiedad"
+      <Propiedad v-for="propiedad in propiedadesFiltradas" :key="propiedad.id" :propiedad="propiedad"
         :price="propertyPrice" />
     </v-row>
   </v-card>
