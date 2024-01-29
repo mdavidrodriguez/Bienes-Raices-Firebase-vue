2812e611c53bfbb01f79f3bfbedc9af16db53a04
mdavidrodriguez <mdavidrodriguez@unicesar.edu.co>
2024-01-29 14:59:27 -0500

    completando edicion

--- src/composables/usePropiedades.js
+++ src/composables/usePropiedades.js
@@ -1,3 +1,5 @@
+import { computed } from "vue";
+
 import { collection } from "firebase/firestore";
 import { useFirestore, useCollection } from "vuefire";
 
@@ -6,7 +8,15 @@ export default function usePropiedades() {
 
   const propiedadesCollection = useCollection(collection(db, "propiedades"));
 
+  const propertyPrice = computed(() => {
+    return (price) =>
+      Number(price).toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+  });
   return {
     propiedadesCollection,
+    propertyPrice,
   };
 }
--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import usePropiedades from '@/composables/usePropiedades'
-const { propiedadesCollection } = usePropiedades()
+const { propiedadesCollection, propertyPrice } = usePropiedades()
 </script>
 <template>
     <h2 class="text-center text-h3 my-5 font-weight-bold">Admin Panel</h2>
@@ -11,17 +11,18 @@ const { propiedadesCollection } = usePropiedades()
         <v-list>
             <v-list-item v-for="propiedad in propiedadesCollection" key="propiedad.id">
                 <template v-slot:prepend>
-                    <v-list-item-media start="true">
-                        <img width="180" :src="propiedad.imagen" alt="">
+                    <v-list-item-media :start="true">
+                        <img width="180" :src="propiedad.imagen">
                     </v-list-item-media>
                 </template>
                 <v-list-item-title>
                     {{ propiedad.titulo }}
                 </v-list-item-title>
-                <v-list-item-subtitle>{{ propiedad.precio }}</v-list-item-subtitle>
+                <v-list-item-subtitle>{{ propertyPrice(propiedad.precio) }}</v-list-item-subtitle>
 
                 <template v-slot:append>
-                    <v-btn color="info" variant="flat" class="mr-2">
+                    <v-btn color="info" variant="flat" class="mr-2"
+                        :to="{ name: 'editar-propiedad', params: { id: propiedad.id } }">
                         Editar
                     </v-btn>
                     <v-btn color="red-darken-3" variant="flat">
